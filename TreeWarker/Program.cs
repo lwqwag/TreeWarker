874801ce7e4e3e4e1ab791c6d7cd1c4d568ab0d3
lwqwag <li2_pei@126.com>
2018-01-08 16:32:04 +0800

    二叉树

--- TreeWarker/Program.cs
+++ TreeWarker/Program.cs
@@ -10,6 +10,16 @@ namespace TreeWarker
     {
         static void Main(string[] args)
         {
+            Tree<int> t = new Tree<int>(1);   //二叉树初始节点  
+            t.Insert(2);
+            t.Insert(-1);
+            t.Insert(3);
+            t.Insert(-3);
+            t.Insert(-2);
+            t.WalkTree();//二叉树的遍历  
+            Console.Read();
         }
     }
+
+
 }
---
+++ TreeWarker/Tree.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TreeWarker
+{
+    public class Tree<TItem> where TItem:IComparable<TItem>
+    {
+        private TItem NodeData;
+        private Tree<TItem> LeftTree;  //左孩子  
+        private Tree<TItem> RightTree;  //右孩子  
+        public Tree(TItem nodeValue)
+        {
+            this.NodeData = nodeValue;//泛型数据  
+            this.LeftTree = null;   //左孩子  
+            this.RightTree = null;   //右孩子  
+        }
+
+        public void Insert(TItem newItem)    //树的插入操作实现二叉排序树  
+        {
+            TItem currentNodeValue = this.NodeData;
+            if (currentNodeValue.CompareTo(newItem) > 0)
+            {
+                if (this.LeftTree == null)
+                {
+                    this.LeftTree = new Tree<TItem>(newItem);
+
+                }
+                else
+                {
+                    this.LeftTree.Insert(newItem);
+                }
+
+            }
+            else
+            {
+                if (this.RightTree == null)
+                {
+                    this.RightTree = new Tree<TItem>(newItem);
+                }
+                else
+                {
+                    this.RightTree.Insert(newItem);
+                }
+            }
+
+        }
+
+        //以下执行左中右的遍历方式  
+
+        public void WalkTree()   //树的遍历  
+        {
+            if (this.LeftTree != null)
+            {
+                this.LeftTree.WalkTree();
+            }
+
+            Console.WriteLine(this.NodeData.ToString());
+
+            if (this.RightTree != null)
+            {
+                this.RightTree.WalkTree();
+            }
+        }
+    }
+}
